06abaae1c3868cabf137049170a7eadfb655a179
Khadizatul Bushra <shaika111225023@gmail.com>
2026-08-29 18:24:31 +0600

    Create Find_Largest_Number.cpp

---
+++ Find_Largest_Number.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a,b,c;
+    cout << "Enter three numbers: ";
+    cin>>a>>b>>c;
+    if (a>b && a>c)
+        cout << " largest ="<<a;
+
+        else if(b>a && b>c)
+        cout << " largest ="<<b;
+
+    else {
+        cout << "largest ="<<c;
+         }
+
+    return 0;
+
+}
